b652c06f60f0af6c2d87e9d7f5bae3292b4fdb9d
utkilic <utkilic@iski.gov.tr>
2025-03-07 09:06:15 +0300

    commit

---
+++ src/main/java/com/teka/dto/DtoStudent.java
@@ -0,0 +1,19 @@
+package com.teka.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DtoStudent
+{
+   private String first_name;
+
+   private String  last_name;
+
+
+}
---
+++ src/main/java/com/teka/dto/DtoStudentIU.java
@@ -0,0 +1,4 @@
+package com.teka.dto;
+
+public class DtoStudentIU {
+}
