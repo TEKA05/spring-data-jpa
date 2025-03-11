aa5b8c89a5d1067321f6e39abcf31a6970e46714
utkilic <utkilic@iski.gov.tr>
2025-03-11 14:55:24 +0300

    @Valid example

--- src/main/java/com/teka/dto/DtoStudentIU.java
+++ src/main/java/com/teka/dto/DtoStudentIU.java
@@ -1,8 +1,7 @@
 package com.teka.dto;
 
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,11 +17,21 @@ public class DtoStudentIU
 {
 
     @NotEmpty(message = "FirstName alanı boş bırakılamaz!")
+    @Min(value = 3 ,message = "first name min 3 karakter olmalıdır")
+    @Max(value = 10, message = "first name max 10 karakter olmalıdır")
     private String first_name;
 
+    @Size(min = 3, max = 30)
     private String  last_name;
 
     private Date birth_of_date;
+
+    @Email(message = "Email formatında bir adres giriniz!")
+    private String email;
+
+    @Size(min = 11 , max =11, message = "T.C. numarası 11 haneli olmalıdır")
+    @NotEmpty(message = "T.C. numarası boş geçilemez")
+    private String tckn;
 
     public String getFirst_name() {
         return first_name;
@@ -46,5 +55,21 @@ public class DtoStudentIU
 
     public void setBirth_of_date(Date birth_of_date) {
         this.birth_of_date = birth_of_date;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTckn() {
+        return tckn;
+    }
+
+    public void setTckn(String tckn) {
+        this.tckn = tckn;
     }
 }
